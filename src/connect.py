f2afad5ddc06d1f6f10ca01b6ca48b25a1efdd15
seongbin <czy4051@uos.ac.kr>
2023-12-04 02:00:56 +0900

    AI server_connect

---
+++ src/connect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+import os
+import webbrowser
+
+webbrowser.open("http://110.34.114.31:5000")
+# session = requests.Session()
+# 서버의 업로드 URL 설정
+# page = requests.get("http://127.0.0.1:5000/check")
+# print(page.text)
