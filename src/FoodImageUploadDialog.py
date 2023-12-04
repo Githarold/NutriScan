1416a6e43f6443906d3d2e97f7e4e1d387bd6723
유승원 <you-seungwon@Simons-MacBookAir.local>
2023-12-04 15:20:24 +0900

    이미지검색후 openapi탐색 후 딕셔너리 업데이트

--- src/FoodImageUploadDialog.py
+++ src/FoodImageUploadDialog.py
@@ -1,7 +1,10 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QComboBox
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QPushButton, QMessageBox, QComboBox
 from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtCore import QUrl
 from datetime import datetime
+import requests
+import webbrowser
+from GetFoodNutrient import Parser
 
 class FoodImageUploadDialog(QDialog):
     def __init__(self, user_credentials, user_id):
@@ -12,15 +15,11 @@ class FoodImageUploadDialog(QDialog):
 
     def initUI(self):
         self.setWindowTitle('음식 사진 업로드')  
-        self.setGeometry(100, 100, 300, 250)  # 창 크기 조정
-        layout = QVBoxLayout()  
+        self.setGeometry(100, 100, 300, 250)
+        layout = QVBoxLayout()
 
-        self.url_input = QLineEdit(self)
-        self.url_input.setPlaceholderText('여기에 이미지 url을 붙여넣기 해주세요')
-        layout.addWidget(self.url_input)
-
-        self.url_button = QPushButton('이미지 url 변환사이트', self)
-        self.url_button.clicked.connect(self.open_image_url_site)
+        self.url_button = QPushButton('이미지 업로드 사이트', self)
+        self.url_button.clicked.connect(self.open_image_upload_site)
         layout.addWidget(self.url_button)
 
         # 식사 시간 선택을 위한 드롭다운 메뉴
@@ -34,23 +33,40 @@ class FoodImageUploadDialog(QDialog):
 
         self.setLayout(layout)
 
-    def open_image_url_site(self):
-        QDesktopServices.openUrl(QUrl("https://ko.imgbb.com"))
+    def open_image_upload_site(self):
+        webbrowser.open("http://110.34.114.31:5000")
 
     def submit_image(self):
-        image_url = self.url_input.text()
         meal_time = self.meal_time_combo.currentText()
-        if not image_url:
-            QMessageBox.warning(self, '입력 오류', '이미지 URL을 입력해주세요.')
-            return
-
-        # 현재 날짜를 얻어오기
         current_date = datetime.now().strftime("%Y-%m-%d")
 
-        # 유저 식단 정보에 이미지 URL 추가
+        # 사용자가 입력한 음식 이름 가져오기
+        food_name = self.get_food_name_from_server()
+
+        # 영양소 정보를 가져오기 위해 Parser 클래스 사용
+        parser = Parser()
+        nutritional_info = parser.get_info_openapi(food_name)
+
+        if nutritional_info["error"]:
+            QMessageBox.warning(self, '오류', nutritional_info["message"])
+            return
+
+        # 유저 식단 정보에 음식 이름과 영양소 정보 추가
         user_diet = self.user_credentials[self.user_id].setdefault('diet', {})
         daily_diet = user_diet.setdefault(current_date, {'아침': [], '점심': [], '저녁': []})
-        daily_diet[meal_time].append({'image_url': image_url})
+        food_info = {
+            'name': food_name,
+            'nutrition': nutritional_info["nutritional_info"]
+        }
+        daily_diet[meal_time].append(food_info)
 
-        QMessageBox.information(self, '이미지 업로드', f'이미지 URL이 {meal_time} 식단에 추가되었습니다.')
+        QMessageBox.information(self, '음식 정보 추가', f'{meal_time} 식단에 "{food_name}"이(가) 추가되었습니다.')
         self.accept()
+
+    def get_food_name_from_server(self):
+        session = requests.Session()
+        response = session.get("http://110.34.114.31:5000/check")
+        if response.ok:
+            return response.text
+        else:
+            return "Unknown"  # 서버로부터 응답이 없을 경우
