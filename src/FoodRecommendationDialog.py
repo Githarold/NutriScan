e31b6fa36924e47918f14adbc8dec36d233493e4
Leetaegyeom <grwise0906@gmail.com>
2023-12-09 12:34:10 +0900

    sorry

--- src/FoodRecommendationDialog.py
+++ src/FoodRecommendationDialog.py
@@ -130,6 +130,63 @@ class FoodRecommendationDialog(QDialog):
             self, "음식 추천", f"{user_name}님을 위한 추천 음식: {recommended_food}"
         )
 
+    def calculate_recommended_intake(self):
+        bmr = self.calculate_bmr()
+        # 탄수화물 50%, 단백질 30%, 지방 20%로 분배
+        carbs_cal = bmr * 0.5
+        protein_cal = bmr * 0.3
+        fats_cal = bmr * 0.2
+
+        # 칼로리를 그램으로 변환 (탄수화물과 단백질 1g당 4칼로리, 지방 1g당 9칼로리)
+        carbs_grams = carbs_cal / 4
+        protein_grams = protein_cal / 4
+        fats_grams = fats_cal / 9
+
+        return {
+            "carbs": round(carbs_grams, 2),
+            "protein": round(protein_grams, 2),
+            "fats": round(fats_grams, 2),
+        }
+
+    def calculate_nutrient_intake(self):
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        user_diet = self.user_credentials.get(self.user_id, {}).get("diet", {})
+
+        nutrient_totals = {"carbs": 0, "protein": 0, "fats": 0}
+        for meal, foods in user_diet.get(current_date, {}).items():
+            for food in foods:
+                if isinstance(food, dict):
+                    nutrition = food.get("nutrition", {})
+                    nutrient_totals["carbs"] += self.safe_convert_to_float(
+                        nutrition.get("탄수화물", 0)
+                    )
+                    nutrient_totals["protein"] += self.safe_convert_to_float(
+                        nutrition.get("단백질", 0)
+                    )
+                    nutrient_totals["fats"] += self.safe_convert_to_float(
+                        nutrition.get("지방", 0)
+                    )
+
+        return nutrient_totals
+
+    def safe_convert_to_float(self, value):
+        try:
+            return float(value)
+        except ValueError:
+            return 0.0
+
+    def calculate_remaining_nutrient_intake(self):
+        recommended_intake = self.calculate_recommended_intake()
+        current_intake = self.calculate_nutrient_intake()
+        remaining_intake = {
+            "carbs": max(recommended_intake["carbs"] - current_intake["carbs"], 0),
+            "protein": max(
+                recommended_intake["protein"] - current_intake["protein"], 0
+            ),
+            "fats": max(recommended_intake["fats"] - current_intake["fats"], 0),
+        }
+        return remaining_intake
+
     # Additional methods for calculating recommended nutrient intake, current nutrient intake, safe conversion to float, and remaining nutrient intake follow the same pattern as described above.
 
     def center(self):
